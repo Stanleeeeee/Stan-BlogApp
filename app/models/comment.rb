e2a2962abfc2c679028f002bea5cc51e27c4bcfa
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-09-03 17:49:51 +0100

    Add processing data in app

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  def comments_posts_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  has_many :likes
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
+  def recent_posts
+    posts.order(created_at: :DESC).limit(3)
+  end
 end
