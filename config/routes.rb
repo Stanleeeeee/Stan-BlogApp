27607565ed0b7898eb56348147f9bc1548f16149
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-09-06 08:55:49 +0100

    Add linters corrections

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: %i[:index show]
-  resources :posts, only: %i[:index show]
+  resources :users, only: %i[index show]
+  resources :posts, only: %i[index show]
 end
