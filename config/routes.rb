dc69a81ba8f8ae17ba9a814bb4dc23587428e7eb
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-09-06 10:44:10 +0100

    Add changes from TSE

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: %i[index show]
-  resources :posts, only: %i[index show]
-end
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
