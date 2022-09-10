e72198c91b3f8e6918fb6b18a8c5d90df7eadfb0
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-09-10 16:02:48 +0100

    Add correction of rubocop error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,15 +8,15 @@ class CommentsController < ApplicationController
     @user = User.where(id: @post.user_id)
     @user.each do |user|
       @comment = @post.comments.new(text: comments_params[:text], user_id: user.id, post_id: @post.id)
+    end
+    redirect_to user_posts_path(id: @post.id, user_id: @post.user_id) if @comment.save
   end
-  redirect_to user_posts_path(id: @post.id, user_id: @post.user_id) if @comment.save
-end
 
   private
 
   def comments_params
     params.require(:comment).permit(:text).tap do |comments_params|
-    comments_params.require(:text)
+      comments_params.require(:text)
     end
   end
 end
