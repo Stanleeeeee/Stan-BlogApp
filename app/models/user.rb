50ce24fcec7f8dc0000d5e0497c723def2191098
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-09-14 20:49:41 +0100

    Add devise functionality in the ususerer er

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 # User model class
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'user_id', class_name: 'Post'
   has_many :comments, foreign_key: 'user_id', class_name: 'Comment'
   has_many :likes, foreign_key: 'user_id', class_name: 'Like'
@@ -7,7 +10,8 @@ class User < ApplicationRecord
   after_save :recent_posts
 
   # validations
-  validates :name, presence: true
+  validates :name, :bio, presence: true
+  validates :email, uniqueness: true
   validates :posts_counter, numericality: { only_integer: true, greater_than: -1 }
 
   def recent_posts
