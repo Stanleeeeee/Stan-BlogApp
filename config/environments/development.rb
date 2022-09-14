191bc585f064263749b2c7972d4e33e32e4dad38
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-09-14 20:58:59 +0100

    Add localhost for devise delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -67,4 +70,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  LetterOpenerWeb.configure do |config|
+    config.letters_location = Rails.root.join('your', 'new', 'path')
+  end
 end
