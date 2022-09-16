d2b87403b64fca4a9022564dc62d4162e426db60
richemonday@gmail.com <richemonday@gmail.com>
2022-09-16 09:00:33 +0100

    made changes to the devise folder

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy, class_name: 'Comment'
   has_many :likes, dependent: :destroy, class_name: 'Like'
 
-
   # validations methods
   validates :title, presence: true, length: { in: 5..250 }
   validates :comments_counter, :likes_counter,
